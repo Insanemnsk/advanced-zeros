aaeaa557369caca200ae1ddd668bbccf51d18eef
Bogdan Kazaryan <bogdankazaryan@yandex.ru>
2018-03-02 02:38:31 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+  let primeAndItsPow = [];
+
+  for (let i = 2; i <= base; i++) {
+    if (base % i === 0) {
+      let pow = 0;
+      while (base % i === 0) {
+        base /= i;
+        pow++;
+      }
+      primeAndItsPow.push([i, pow]);
+    }
+  }
+
+  function getZerosAmount(prime, pow) {
+    let count = 0;
+    let primeTimes = prime;
+    while (primeTimes < number) {
+      count += Math.floor(number / primeTimes);
+      primeTimes *= prime;
+    }
+    return Math.floor(count / pow);
+  }
+
+  return Math.min(...primeAndItsPow.map(pair => getZerosAmount(...pair)));
+};
